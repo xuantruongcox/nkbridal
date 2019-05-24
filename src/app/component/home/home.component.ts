f444ba8752c8a83ca719a538eb41a0d7c8b27e0a
truong <truong@localhost.localdomain>
2019-05-24 16:52:18 +0700

    RESPONSIVE

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,18 +1,32 @@
 import { DragScrollModule, DragScrollComponent } from 'ngx-drag-scroll';
 import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { NgImageSliderComponent } from 'ng-image-slider';
+import * as $ from 'jquery';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 
 })
-export class HomeComponent {
-  @ViewChild('nav', {read: DragScrollComponent})ds: DragScrollComponent;
-  moveLeft(){
-    this.ds.moveLeft();
+export class HomeComponent implements OnInit {
+  ngOnInit() {
+    $('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
+      var next = $(this).next();
+      if (!next.length) {
+        next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+      for (var i=0;i<4;i++) {
+        next=next.next();
+        if (!next.length) {
+          next=$(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+      }
+    });
   }
-  moveRight(){
-    this.ds.moveRight();
+
+  constructor() {
   }
 }
+
